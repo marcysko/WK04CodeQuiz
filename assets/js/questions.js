e458872286a4b415bfe262449d56cd50dcf4a040
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-02-18 15:49:54 -0700

    add quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,55 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        title: "Who invented JavaScript?",
+        choices: ["Bill Gates", "Pat Sajak", "Janet Jackson", "Brendan Eich"],
+        answer: "Brendan Eich"
+    },
+    {
+        title: "What tags do you use to add Javascript to a website?",
+        choices: ["<style>", "<script>", "<html>", "<body>"],
+        answer: "<script>"
+    },
+    {
+        title: "What is Vanilla Javascript?",
+        choices: ["coffee ice cream", "frappuccino", "plain JavaScript", "caffeine shot"],
+        answer: "plain JavaScript"
+    },
+    {
+        title: "What is the correct way to enter a comment into JavaScript?",
+        choices: [
+            "// My pet alligator ate my sandwich",
+            "<!-- It is friday -->",
+            "/* It's my birthday and I'm working */",
+            "All of the above"
+          ],
+          answer: "// My pet alligator ate my sandwich"
+        },
+        {
+            title:
+            "Why do we use JavaScript debuggers?",
+            choices: [
+            "to kill the fly that landed on our monitor",
+            "to get the kids out of the room while we're coding",
+            "clean off our windshield",
+            "search for and fix errors in programming code"
+          ],
+            answer: "search for and fix errors in programming code"
+        },
+        { 
+            title: "What is an example of a JavaScript Array?",
+            choices: [
+            "var dogs = ["Beagle", "Poodle", "Dachsund"]",
+            "var x = "Billy Budd";",
+            "var d = new Date();",
+            "let language = "JavaScript";"
+            ],
+            answer: "var dogs = ["Beagle", "Poodle", "Dachsund"]"
+        },
+        {
+          title: "What type of Pop up boxes are available in JavaScript?:",
+          choices: ["Alert", "Confirm", "Prompt", "All the above"],
+          answer: "All the above"
+        }
+      ];
+    
